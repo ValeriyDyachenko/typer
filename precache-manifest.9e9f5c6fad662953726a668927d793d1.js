c187b483c5daf784186d366e5eb9eb6d46923b2b
Valerii Diachenko <spamogenerator@gmail.com>
2019-08-20 23:34:18 +0300

    Updates

---
+++ precache-manifest.9e9f5c6fad662953726a668927d793d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efda2d59dbe7c33b4ddef17ad86511be",
+    "url": "/typer/index.html"
+  },
+  {
+    "revision": "754fb78741bfaef1f1c9",
+    "url": "/typer/static/css/main.a99e7aba.chunk.css"
+  },
+  {
+    "revision": "489b87ae90ea2e7c2281",
+    "url": "/typer/static/js/2.4df4607d.chunk.js"
+  },
+  {
+    "revision": "754fb78741bfaef1f1c9",
+    "url": "/typer/static/js/main.449490a8.chunk.js"
+  },
+  {
+    "revision": "bf493d931a21f6f6fd71",
+    "url": "/typer/static/js/runtime~main.24816a52.js"
+  }
+]);
